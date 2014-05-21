e130e8fd2f190caff6d0a16c02ab510cf7c3e2e0
Baron Chandler <baron@venturecranial.com>
2014-05-20 21:16:36 -0400

    ....mmmmm....

--- env.sh
+++ env.sh
@@ -26,7 +26,7 @@ if [ "x$OS" = "xDarwin" ]; then
     export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
 fi
 
-export PIP_DOWNLOAD_CACHE=.pip
+export PIP_DOWNLOAD_CACHE=../.pip
 mkdir -p $PIP_DOWNLOAD_CACHE && chmod a+t $PIP_DOWNLOAD_CACHE
 pip install -r requirements.txt
 
